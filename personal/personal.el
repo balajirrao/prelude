e35b8d847269b6727bda57c67acbd0a795cb7098
Balaji Rao <balaji.rao@redpineapplemedia.com>
2017-12-29 19:25:23 +0100

    Some files

---
+++ personal/personal.el
@@ -0,0 +1,8 @@
+(setq default-frame-alist '((font . "Fira Code-12")))
+
+(prelude-require-package 'symbol-overlay)
+(global-set-key (kbd "M-i") 'symbol-overlay-put)
+(global-set-key (kbd "M-n") 'symbol-overlay-switch-forward)
+(global-set-key (kbd "M-p") 'symbol-overlay-switch-backward)
+(global-set-key (kbd "<f7>") 'symbol-overlay-mode)
+(global-set-key (kbd "<f8>") 'symbol-overlay-remove-all)
