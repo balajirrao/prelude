bf1683a573086f8afe55e48c49155cc73bae715e
Balaji Rao <balaji.rao@redpineapplemedia.com>
2018-02-16 12:17:18 +0100

    Rebase

--- personal/personal.el
+++ personal/personal.el
@@ -1,5 +1,12 @@
 (setq default-frame-alist '((font . "Fira Code-12")))
 
+(disable-theme 'zenburn)
+
+(prelude-require-package 'color-theme-sanityinc-tomorrow)
+(prelude-require-package 'sublime-themes)
+(prelude-require-package 'solarized-theme)
+(prelude-require-package 'monokai-theme)
+
 (prelude-require-package 'symbol-overlay)
 (global-set-key (kbd "M-i") 'symbol-overlay-put)
 (global-set-key (kbd "M-n") 'symbol-overlay-switch-forward)
